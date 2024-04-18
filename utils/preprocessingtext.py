1eb3e8c1d7b2f30dfc5c42bbc82cfff69943fedf
Pruthvi Patel <patelpruthvi.01221@gmail.com>
2024-04-17 23:24:11 -0400

    FIx nltk requirements

--- utils/preprocessingtext.py
+++ utils/preprocessingtext.py
@@ -11,6 +11,11 @@ stopwords_path = os.path.join(os.path.dirname(__file__), '..', 'nltk_data', 'sto
 with open(stopwords_path, 'r') as file:
     stop_words = set(file.read().splitlines())
 
+# write logic to not download if already downloaded for server deployment, if not, downloaded save in nltk_data folder
+if not os.path.exists(os.path.join(os.path.dirname(__file__), '..', 'nltk_data', 'punkt')):
+    nltk.download('punkt', download_dir=os.path.join(os.path.dirname(__file__), '..', 'nltk_data'))
+
+
 def preprocess_text(text):
     text = text.lower()
     text = re.sub(r'[^a-zA-Z\s]', '', text)

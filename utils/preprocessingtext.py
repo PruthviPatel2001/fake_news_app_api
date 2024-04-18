5ad8b7743251a5e2b1cfb11c9a6a4d55256763c4
Pruthvi Patel <patelpruthvi.01221@gmail.com>
2024-04-17 23:35:56 -0400

    Fix nltk error

--- utils/preprocessingtext.py
+++ utils/preprocessingtext.py
@@ -4,16 +4,12 @@ from nltk.stem import PorterStemmer
 import os
 
 stemmer = PorterStemmer()
-
+nltk.download('punkt')
 
 stopwords_path = os.path.join(os.path.dirname(__file__), '..', 'nltk_data', 'stopwords.txt')
 
 with open(stopwords_path, 'r') as file:
     stop_words = set(file.read().splitlines())
-
-# write logic to not download if already downloaded for server deployment, if not, downloaded save in nltk_data folder
-if not os.path.exists(os.path.join(os.path.dirname(__file__), '..', 'nltk_data', 'punkt')):
-    nltk.download('punkt', download_dir=os.path.join(os.path.dirname(__file__), '..', 'nltk_data'))
 
 
 def preprocess_text(text):
